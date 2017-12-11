39bd53883969a6acd9871f18fe132ccfa323c50b
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2017-12-11 23:06:11 +0600

    Add files via upload

---
+++ ostringstream.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main (void)
+{
+
+    ostringstream os;
+    double x = 3.14159;
+    os << x;
+    string s = os.str();
+    cout << s << endl;
+
+    return 0;
+}
