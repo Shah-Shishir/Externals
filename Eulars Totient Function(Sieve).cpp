6ca3054c6c30a2e8c8c3a1c7992eea5b80bf6a21
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2018-04-14 01:11:11 +0600

    Add files via upload

--- Eulars Totient Function(Sieve).cpp
+++ Eulars Totient Function(Sieve).cpp
@@ -4,9 +4,8 @@ We know, The totient function of a number N can be determined,
 phi(N) = N * (1-(1/p1)) * (1-(1/p2)) * ....... (1-(1/pn));
 where p1,p2,...,pn are distinct prime factors of N.
 
-So, we run a loop from 1 to n and store the ith value in the ith index of phi[]. Then run another loop from 2 to N and check if i
-be prime or not. If i be prime, we will set phi[i]=i-1 and then run a nested loop to set its multiples totient value. Each time a
-prime number p appears, it will multiply its all multiples (phi[i]) by (1-(1/p)).
+So, we run a loop from 2 to n and check if i be prime or not. If i be prime, we will set phi[i]=i-1 and then run a nested loop
+to set its multiples totient value. Each time a prime number p appears, it will multiply its all multiples (phi[i]) by (1-(1/p)).
 
 Then after the end of the loop, we will get our all totient value upto N.
 
@@ -29,17 +28,19 @@ void totient_func ()
 {
     int i,j;
 
-    for (i=1; i<=MAX; i++)
-        phi[i] = i;
-
     for (i=2; i<=MAX; i++)
     {
-        if (phi[i] == i)
+        if (phi[i] == 0)
         {
             phi[i] = i-1;
 
             for (j=2*i; j<=MAX; j+=i)
+            {
+                if (phi[j] == 0)
+                    phi[j] = j;
+
                 phi[j] = (phi[j]/i)*(i-1);
+            }
         }
     }
 }
